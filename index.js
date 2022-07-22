c2bbdc20d6b4d86023900f57c3bcf7e8fec8c7b5
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-22 04:59:49 -0700

    commit tree a4d07fa6295b9d5b2d179983afcc2de1b77dc5e3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function fetchBooks() {
+function fetchBooks(){
+ return fetch('https://anapioficeandfire.com/api/books')
+ .then(res => res.json())
+  .then(book =>renderBooks(book))
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach (book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -14,4 +16,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
+
